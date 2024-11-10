3cb62251c0de0c5268d209bbb8b7ec8f45595c66
cameron <thompsonca99@gmail.com>
2024-11-09 22:47:05 -0600

    update depenencies

--- seed.ts
+++ seed.ts
@@ -1,9 +1,6 @@
 import { getDataSource } from 'data-source';
-import { Work } from './src/work/Work.entity';
 import { Blog } from './src/blog/Blog.entity';
 import { About } from './src/about/About.entity';
-import { GameDev } from './src/game-dev/Game-dev.entity';
-import { Cooking } from './src/cooking/Cooking.entity';
 
 async function seed() {
   // Initialize the data source
@@ -24,29 +21,8 @@ async function seed() {
   console.log('Database connected');
 
   // Repositories for each entity
-  const workRepository = AppDataSource.getRepository(Work);
   const blogRepository = AppDataSource.getRepository(Blog);
   const aboutRepository = AppDataSource.getRepository(About);
-  const gameDevRepository = AppDataSource.getRepository(GameDev);
-  const cookingRepository = AppDataSource.getRepository(Cooking);
-
-  // Seed data for each entity
-  const workData = [
-    {
-      company: 'Tech Company',
-      description: 'Developed web applications',
-      startDate: new Date('2022-01-01'),
-      endDate: new Date('2023-01-01'),
-      isPersonalProject: false,
-    },
-    {
-      company: 'Another Company',
-      description: 'Built and maintained UI components',
-      startDate: new Date('2021-01-01'),
-      endDate: new Date('2022-01-01'),
-      isPersonalProject: false,
-    },
-  ];
 
   const blogData = [
     {
@@ -69,41 +45,13 @@ async function seed() {
     content: 'I am a software engineer with a passion for coding...',
   };
 
-  const gameDevData = [
-    {
-      title: 'First Game',
-      description: 'An RPG game with exciting features',
-      releaseDate: new Date('2022-06-01'),
-      features: 'RPG',
-    },
-  ];
-
-  const cookingData = [
-    {
-      title: 'Spaghetti Bolognese',
-      ingredients: 'Spaghetti, Tomato, Beef, Onion, Garlic',
-      instructions: 'Cook spaghetti. Prepare sauce. Mix and serve.',
-      createdAt: new Date(),
-    },
-  ];
-
-  console.log('\n BEFORE \n');
   // Insert data into each table
-  await workRepository.save(workData);
-  console.log('Work data seeded');
 
-  console.log('\n AFTER \n');
   await blogRepository.save(blogData);
   console.log('Blog data seeded');
 
   await aboutRepository.save(aboutData);
   console.log('About data seeded');
-
-  await gameDevRepository.save(gameDevData);
-  console.log('GameDev data seeded');
-
-  await cookingRepository.save(cookingData);
-  console.log('Cooking data seeded');
 
   // Close the data source
   await AppDataSource.destroy();
